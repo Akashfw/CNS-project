5133c755c391a619b2160b08b147081eea346e04
Akashfw <shuklaakash30278@gmail.com>
2023-08-14 20:20:16 +0530

    project done-1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose= require("mongoose")
 
-const dbURI = "mongodb+srv://akash:shukla@cluster0.zvutwxw.mongodb.net/";
+const dbURI = "mongodb+srv://akash:shukla@cluster0.zvutwxw.mongodb.net/test";
 const connection= mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
